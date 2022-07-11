c6328940ca3c686b7788d355dd30940f7d8373b0
teanus <Gamer.57dima@gmail.com>
2022-07-11 06:09:49 +0600

    теперь список заблокированных команд хранится в бд

--- provider/sqlite_db.py
+++ provider/sqlite_db.py
@@ -1,34 +1,37 @@
-#           Free версия бота проекта LostWeyn
-#               Telegram: t.me/lostweyn_project
-#  #
+#
 #           Контакты разработчика:
 #               VK: vk.com/dimawinchester
 #               Telegram: t.me/teanus
 #               Github: github.com/teanus
-#               24serv: talk.24serv.pro/u/teanus
-#  #
-#  #
-#      ██╗      ██████╗ ███████╗████████╗██╗    ██╗███████╗██╗   ██╗███╗   ██╗
-#      ██║     ██╔═══██╗██╔════╝╚══██╔══╝██║    ██║██╔════╝╚██╗ ██╔╝████╗  ██║
-#      ██║     ██║   ██║███████╗   ██║   ██║ █╗ ██║█████╗   ╚████╔╝ ██╔██╗ ██║
-#      ██║     ██║   ██║╚════██║   ██║   ██║███╗██║██╔══╝    ╚██╔╝  ██║╚██╗██║
-#      ███████╗╚██████╔╝███████║   ██║   ╚███╔███╔╝███████╗   ██║   ██║ ╚████║
-#      ╚══════╝ ╚═════╝ ╚══════╝   ╚═╝    ╚══╝╚══╝ ╚══════╝   ╚═╝   ╚═╝  ╚═══╝
+#
+#
+#
+# ████████╗███████╗ █████╗ ███╗   ██╗██╗   ██╗███████╗
+# ╚══██╔══╝██╔════╝██╔══██╗████╗  ██║██║   ██║██╔════╝
+#    ██║   █████╗  ███████║██╔██╗ ██║██║   ██║███████╗
+#    ██║   ██╔══╝  ██╔══██║██║╚██╗██║██║   ██║╚════██║
+#    ██║   ███████╗██║  ██║██║ ╚████║╚██████╔╝███████║
+#    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
 
 
-from Config import db_name
-from locales import lang
+from resources import config
 import sqlite3 as sql
+
+db = None
+cur = None
 
 
 def sql_start():
     global db, cur
-    db = sql.connect(db_name)
+    db = sql.connect(config.database()['name'])
     cur = db.cursor()
     if db:
         print('Sqlite3: connected')
-    query = 'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, telegram_id TEXT, role TEXT DEFAULT "normal")'
-    cur.execute(query)
+    table_users = 'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, telegram_id TEXT, role TEXT DEFAULT ' \
+                  '"normal") '
+    table_black_list = 'CREATE TABLE IF NOT EXISTS black_list(command TEXT)'
+    cur.execute(table_users)
+    cur.execute(table_black_list)
     db.commit()
 
 
@@ -37,7 +40,7 @@ def user_add(user_id):
     query = 'INSERT INTO users(telegram_id, role) VALUES(?, ?)'
     cur.execute(query, [user_id, 'normal'])
     db.commit()
-    return lang.message_sqlite_add_normal
+    return 'Пользователь был добавлен в список'
 
 
 def admin_add(user_id):
@@ -45,14 +48,63 @@ def admin_add(user_id):
     query = 'INSERT INTO users(telegram_id, role) VALUES(?, ?)'
     cur.execute(query, [user_id, 'admin'])
     db.commit()
-    return lang.message_sqlite_add_admin
+    return 'Админ был добавлен в список'
+
+
+def user_remove(user_id):
+    sql_start()
+    query = 'DELETE FROM users WHERE telegram_id = ?'
+    cur.execute(query, user_id)
+    db.commit()
+    return 'Пользователь был успешно удален из бд'
+
+
+def admin_remove(user_id):
+    sql_start()
+    query = 'DELETE FROM users WHERE telegram_id= ?'
+    cur.execute(query, user_id)
+    db.commit()
+    return 'Администратор успешно удален из бд!'
+
+
+def add_black_list(cmd):
+    sql_start()
+    query = 'INSERT INTO black_list(command) VALUES(?)'
+    cur.execute(query, cmd)
+    db.commit()
+    return 'Команда была заблокирована'
+
+
+def remove_black_list(cmd):
+    sql_start()
+    query = f"DELETE FROM black_list WHERE command = ?"
+    cur.execute(query, cmd)
+    db.commit()
+    return 'Команда была разблокирована'
+
+
+def commands_all():
+    sql_start()
+    query = 'SElECT * FROM black_list'
+    result = cur.execute(query).fetchall()
+    text = str(result)[1:-1]
+    symbols = ['(', ')', ',', '\'']
+    for char in symbols:
+        text = text.replace(char, '').replace(' ', '\n')
+    return text
+
+
+def command_exists(cmd):
+    sql_start()
+    query = 'SELECT * FROM black_list WHERE command = ?'
+    result = cur.execute(query, (cmd,)).fetchall()
+    return bool(len(result))
 
 
 def user_exists(user_id):
     sql_start()
     query = 'SELECT * FROM users WHERE telegram_id = ?'
     result = cur.execute(query, (user_id,)).fetchall()
-    db.commit()
     return bool(len(result))
 
 
@@ -60,5 +112,4 @@ def check_admin_user(user_id):
     sql_start()
     query = f'SELECT role FROM users WHERE telegram_id = ? AND role == ?'
     result = cur.execute(query, (user_id, 'admin')).fetchall()
-    db.commit()
     return bool(len(result))
