83d18189be04086300998ed6688b8ccf3ccd43aa
teanus <Gamer.57dima@gmail.com>
2023-08-27 01:40:49 +0600

    исправил ошибку, закинул обработчик логов

---
+++ logger/log.py
@@ -0,0 +1,31 @@
+#
+#           Контакты разработчика:
+#               VK: vk.com/dimawinchester
+#               Telegram: t.me/teanus
+#               Github: github.com/teanus
+#
+#
+#
+# ████████╗███████╗ █████╗ ███╗   ██╗██╗   ██╗███████╗
+# ╚══██╔══╝██╔════╝██╔══██╗████╗  ██║██║   ██║██╔════╝
+#    ██║   █████╗  ███████║██╔██╗ ██║██║   ██║███████╗
+#    ██║   ██╔══╝  ██╔══██║██║╚██╗██║██║   ██║╚════██║
+#    ██║   ███████╗██║  ██║██║ ╚████║╚██████╔╝███████║
+#    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.INFO)
+
+log_filename = "info.log"
+file_handler = logging.FileHandler(log_filename)
+
+formatter = logging.Formatter(
+    "%(asctime)s - TeaLogger - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
