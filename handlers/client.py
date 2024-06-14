f4476378589963ab4b2f7a5135a38e24c86fe2dc
teanus <Gamer.57dima@gmail.com>
2024-06-14 06:10:06 +0500

    black 8

--- handlers/client.py
+++ handlers/client.py
@@ -36,7 +36,9 @@ async def rcon_cmd(message: types.Message) -> None:
     is_admin = await db.check_admin_user(chat_id)
     if is_admin or await db.user_exists(chat_id):
         role = "администратора" if is_admin else '"normal"'
-        logger.info(f"Пользователь с id {user_id} вошел в rcon консоль с правами {role}")
+        logger.info(
+            f"Пользователь с id {user_id} вошел в rcon консоль с правами {role}"
+        )
         await message.reply("Теперь пришли команду", reply_markup=kb_client.rcon_cancel)
         await FsmOther.rcon.set()
     else:
@@ -62,7 +64,9 @@ async def get_command(message: types.Message) -> None:
     command = low.split(" ", 1)
 
     if not await db.check_admin_user(chat_id) and await db.command_exists(command[0]):
-        logger.info(f"Пользователь с id {user_id} попытался выполнить заблокированную команду")
+        logger.info(
+            f"Пользователь с id {user_id} попытался выполнить заблокированную команду"
+        )
         await groups_logger("RCON: ", user_id, message.text)
         await message.reply("Команда заблокирована! Используйте другую:)")
     else:
@@ -71,7 +75,9 @@ async def get_command(message: types.Message) -> None:
         logger.info(f"{role} с id {user_id} выполнил команду: {message.text}")
         await groups_logger("RCON: ", user_id, message.text)
         await message.reply(f"Команда выполнена. Ответ сервера:\n{result}")
-        await message.answer("Вы можете продолжить выполнять команды. Просто пришлите мне их. Или введите отмена")
+        await message.answer(
+            "Вы можете продолжить выполнять команды. Просто пришлите мне их. Или введите отмена"
+        )
         await FsmOther.rcon.set()
 
 
