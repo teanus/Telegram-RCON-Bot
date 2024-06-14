1cb7af331c9aa8c3be02e7b1d278213e447cc334
teanus <Gamer.57dima@gmail.com>
2024-06-14 06:18:03 +0500

    упростил

--- add_super_admin.py
+++ add_super_admin.py
@@ -19,16 +19,15 @@ from resources import config
 
 
 async def console_add_super_admin() -> str:
-    if config.console()["give_role"]:
-        admin_id = input(
-            "Введите id для выдачи прав super-админа или нажмите Enter для пропуска: "
-        )
-        if admin_id == "":
-            return "Закрытие"
-        elif await db.check_admin_user(admin_id):
-            return admin_id + " уже есть в списке супер-админов"
-        else:
-            await db.add_admin(admin_id)
-            return admin_id + " был добавлен в список супер-админов"
-    else:
+    if not config.console()["give_role"]:
         return "Режим выдачи роли выключен. Пропускаем"
+
+    admin_id = input("Введите id для выдачи прав super-админа или нажмите Enter для пропуска: ")
+    if admin_id == "":
+        return "Закрытие"
+
+    if await db.check_admin_user(admin_id):
+        return f"{admin_id} уже есть в списке супер-админов"
+
+    await db.add_admin(admin_id)
+    return f"{admin_id} был добавлен в список супер-админов"
