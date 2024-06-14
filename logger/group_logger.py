80652780b86c9171672a4f9d40f97856735d1ca0
teanus <Gamer.57dima@gmail.com>
2024-06-14 06:16:20 +0500

    упростил

--- logger/group_logger.py
+++ logger/group_logger.py
@@ -20,12 +20,7 @@ from resources import config
 
 
 async def groups_logger(prefix: str, user_id: str, message: str) -> None:
-    if not config.telegram()["on_logger_group"]:
-        return
-    else:
-        if await db.check_admin_user(user_id):
-            message = f"{prefix} Админ с ID {user_id} - ввел команду: {message}"
-            await bot.send_message(config.telegram()["logger_chat_id"], message)
-        else:
-            message = f"{prefix} Пользователь с ID {user_id} - ввел команду: {message}"
-            await bot.send_message(config.telegram()["logger_chat_id"], message)
+    if config.telegram()["on_logger_group"]:
+        user_type = "Админ" if await db.check_admin_user(user_id) else "Пользователь"
+        log_message = f"{prefix} {user_type} с ID {user_id} - ввел команду: {message}"
+        await bot.send_message(config.telegram()["logger_chat_id"], log_message)
