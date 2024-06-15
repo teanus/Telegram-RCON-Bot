7eef7a58fd2f5eb064ee239747a1684e4336cfaf
teanus <Gamer.57dima@gmail.com>
2024-06-15 15:31:36 +0500

    don`t write

--- create_bot.py
+++ create_bot.py
@@ -1,5 +1,5 @@
 #
-#           Контакты разработчика:
+#           Контакты разработчика
 #               VK: vk.com/dimawinchester
 #               Telegram: t.me/teanus
 #               Github: github.com/teanus
