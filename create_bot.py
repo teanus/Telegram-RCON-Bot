5024a5c64ec1e07a22688f6f6db50e4b0ac3a86a
teanus <Gamer.57dima@gmail.com>
2024-06-15 14:09:00 +0500

    вернул что удалил случайно

---
+++ create_bot.py
@@ -0,0 +1,27 @@
+#
+#           Контакты разработчика:
+#               VK: vk.com/dimawinchester
+#               Telegram: t.me/teanus
+#               Github: github.com/teanus
+#
+#
+#
+# ████████╗███████╗ █████╗ ███╗   ██╗██╗   ██╗███████╗
+# ╚══██╔══╝██╔════╝██╔══██╗████╗  ██║██║   ██║██╔════╝
+#    ██║   █████╗  ███████║██╔██╗ ██║██║   ██║███████╗
+#    ██║   ██╔══╝  ██╔══██║██║╚██╗██║██║   ██║╚════██║
+#    ██║   ███████╗██║  ██║██║ ╚████║╚██████╔╝███████║
+#    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
+
+
+import os
+
+from aiogram import Bot
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import Dispatcher
+from dotenv import load_dotenv
+
+load_dotenv()
+storage = MemoryStorage()
+bot = Bot(os.getenv("TOKEN"))
+dp = Dispatcher(storage=storage)
