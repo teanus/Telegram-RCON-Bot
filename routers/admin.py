1a774ba12b946756098a35dd9a8b8df75009c41d
teanus <Gamer.57dima@gmail.com>
2024-07-31 02:50:04 +0500

    black

--- routers/admin.py
+++ routers/admin.py
@@ -196,7 +196,9 @@ async def get_add_admin_id(message: types.Message) -> None:
     chat_id = message.chat.id
     text_id = message.text
     if not text_id.isdigit():
-        await message.reply("Это не является ID, id должен содержать ТОЛЬКО цифры и ничего другого. Пример: 78715102429")
+        await message.reply(
+            "Это не является ID, id должен содержать ТОЛЬКО цифры и ничего другого. Пример: 78715102429"
+        )
     elif await db.check_admin_user(message.text):
         await message.answer(
             "Этот id уже имеет роль администратора. Введите другой id или нажмите 'назад'"
