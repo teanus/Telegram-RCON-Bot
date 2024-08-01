2149f47e29e2b774c25d7404000ccd985e6b4c89
teanus <Gamer.57dima@gmail.com>
2024-08-02 04:49:27 +0500

    Теперь имеет свой шаблон текста

--- logger/group_logger.py
+++ logger/group_logger.py
@@ -18,13 +18,22 @@ from aiogram.exceptions import TelegramAPIError
 
 from create_bot import bot
 from provider import db
+from render_template import render_template_jinja
 from resources import config
 
 
 async def groups_logger(prefix: str, user_id: int, message: str) -> None:
     if config.telegram().get("on_logger_group"):
-        user_type = "Админ" if await db.check_admin(user_id) else "Пользователь"
-        log_message = f"{prefix} {user_type} с ID {user_id} - ввел команду: {message}"
+        is_admin = await db.check_admin(user_id)
+        context = {
+            "prefix": prefix,
+            "is_admin": is_admin,
+            "user_id": user_id,
+            "message": message,
+        }
+        log_message = render_template_jinja(
+            "messages.jinja2", "template/group_logger", **context
+        )
         try:
             await bot.send_message(config.telegram().get("logger_chat_id"), log_message)
         except TelegramAPIError as error:
