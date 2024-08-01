e77692aa71143454a2eddc5c012d24e9d1e1f3a8
teanus <Gamer.57dima@gmail.com>
2024-08-02 04:26:40 +0500

    black

--- render_template/__init__.py
+++ render_template/__init__.py
@@ -1,5 +1,2 @@
-from render_template.render import (
-    load_keyboards,
-    load_valid_commands,
-    render_template_jinja,
-)
+from render_template.render import (load_keyboards, load_valid_commands,
+                                    render_template_jinja)
