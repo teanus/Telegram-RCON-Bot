3b0b5dce99d0ad1d67380d68116be7826748b9b5
teanus <Gamer.57dima@gmail.com>
2024-08-01 22:21:27 +0500

    исправил неполадки, улучшил varname

--- logger/group_logger.py
+++ logger/group_logger.py
@@ -23,7 +23,7 @@ from resources import config
 
 async def groups_logger(prefix: str, user_id: int, message: str) -> None:
     if config.telegram().get("on_logger_group"):
-        user_type = "Админ" if await db.check_admin_user(user_id) else "Пользователь"
+        user_type = "Админ" if await db.check_admin(user_id) else "Пользователь"
         log_message = f"{prefix} {user_type} с ID {user_id} - ввел команду: {message}"
         try:
             await bot.send_message(config.telegram().get("logger_chat_id"), log_message)
